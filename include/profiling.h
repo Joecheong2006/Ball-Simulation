ffd19c53cb2c48fb9867a4fc955c83b4adc14461
LVOrac <joecheong2006@gmail.com>
2026-08-23 14:17:57 +0800

    Added Macros

--- include/profiling.h
+++ include/profiling.h
@@ -2,4 +2,10 @@
 
 #if defined(TRACY_ENABLE)
     #include <tracy/Tracy.hpp>
+#else
+    #define ZoneScoped
+    #define ZoneScopedN
+    #define FrameMark
+    #define FrameMarkStart
+    #define FrameMarkEnd
 #endif
